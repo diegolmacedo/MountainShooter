85fbff3f0ac469a0d5057604d786dc1f5e6a9413
Diego L. Macedo <84526549+diegolmacedo@users.noreply.github.com>
2026-05-06 20:18:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print('Setup End')
 
 print('Loop Start')
 while True:
-    # Check for all events
+    # check for all events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() # close window
